f253f0a957fba8fd9064fc6cf921d4e004999c08
Łukasz Strzelczyk <lukaszmiloblad@gmail.com>
2025-02-07 01:05:47 +0100

    start testowego plotu

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,20 @@
-fn main() {
-    println!("Hello, world!");
+use plotters::prelude::*;
+
+fn main() {}
+
+fn fake_data() -> Result<(), Box<dyn std::error::Error>> {
+    let root = BitMapBackend::new("plots/plot1.png", (640, 480)).into_drawing_area();
+    root.fill(&WHITE)?;
+    let mut chart = ChartBuilder::on(&root)
+        .caption("test plot", ("sans-seriff", 50).into_font())
+        .margin(5)
+        .x_label_area_size(30)
+        .y_label_area_size(30)
+        .build_cartesian_2d(-1f32..1f32, -0.1f32..1f32)?;
+    chart.configure_mesh().draw()?;
+    chart.draw_series(LineSeries::new(
+        (-50..=50).map
+    ))
+    
+    Ok(())
 }
